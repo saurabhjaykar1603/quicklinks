06e6928e810fbdd4503b42f7791a6ff12ebd9273
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2023-11-09 12:18:57 +0530

    ref : model file name

---
+++ server/models/Link.js
@@ -0,0 +1,24 @@
+import { Schema, model } from "mongoose";
+
+const linkSchema = new Schema({
+  url: {
+    type: "string",
+    required: true,
+  },
+
+  slug: {
+    type: "string",
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+
+  clicks: {
+    type: Number,
+    default: 0,
+    require: true
+  },
+});
+
+const Link = model("Link", linkSchema);
+export default Link;
