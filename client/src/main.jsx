ca84007666d19b701ef0eff534ab2fac4be0c886
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2023-11-13 13:29:05 +0530

    feat : routing added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Home from "./views/Home/Home";
+import About from "./views/About/About";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Signup from "./views/Signup/Signup";
+import Login from "./views/Login/Login";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <>
-    <App />
-  </>,
-)
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/about", element: <About /> },
+  { path: "/signup", element: <Signup /> },
+  { path: "/login", element: <Login /> },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
