20805d1b48dff92f6a38fed74bc54492b7cb5f60
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2023-11-16 17:12:12 +0530

    feat: Add Short URL Generation Form

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -1,12 +1,43 @@
-import React from 'react'
-import "./Home.css"
+import React from "react";
+import "./Home.css";
+import Navbar from "../../components/Navbar/Navbar";
+import copyPng from "./images/copy.png";
 
 function Home() {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <div className="sticky-top">
+        <Navbar />
+      </div>
+      <div className="d-flex flex-wrap mt-5 align-content-center justify-content-evenly">
+        <div>
+          <form class="form">
+            <p class="form-title">Generate Short Url Here</p>
+            <div class="input-container">
+              <input placeholder="Enter url" type="email" required />
+            </div>
+            <div class="input-container">
+              <input placeholder="Enter short url or slug " type="text" />
+            </div>
+            <div class="input-container">
+              <input placeholder="Enter short " type="text" readOnly />
+              <span>
+                <img src={copyPng} alt="png" className="h-25"/>
+              </span>
+            </div>
+            <button class="submit" type="button">
+              Do Magic
+            </button>
+          </form>
+        </div>
+        <div className="">
+          <div className="card m-0 m-md-4" style={{ width: "22rem" }}>
+            <div className="card-body"></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Home
+export default Home;
