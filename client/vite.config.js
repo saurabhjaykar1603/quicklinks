c75af172bd7ae417c2178a6b044bf331620ee1dd
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2023-11-17 12:00:48 +0530

    chore(config): add server proxy for API request

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": "http://localhost:8080",
+    },
+  },
   plugins: [react()],
-})
+});
