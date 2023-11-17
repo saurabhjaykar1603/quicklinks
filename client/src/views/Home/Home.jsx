64d5ac0165f12dd167fcfef0cf02b5760844918e
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2023-11-17 12:08:14 +0530

    feat: implement link generation and copy functionality

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import "./Home.css";
 import Navbar from "../../components/Navbar/Navbar";
 import copyPng from "./images/copy.png";
+import axios from "axios";
 
 function Home() {
+  const [url, setUrl] = useState("");
+  const [slug, setSlug] = useState("");
+  const [shortUrl, setShortUrl] = useState("");
+
+  // generate link
+
+  const generateLink = async () => {
+    const response = await axios.post("/api/links", {
+      url,
+      slug,
+    });
+
+    setShortUrl(response?.data?.data?.shortUrl);
+  };
+
+  const urlRef = useRef(null); // create input ref
+
+  const copyShortUrl = useCallback(() => {
+    urlRef.current?.select(); // Select the text inside the input element
+    window.navigator.clipboard.writeText(shortUrl);
+  }, [shortUrl]);
+
   return (
     <>
       <div className="sticky-top">
@@ -11,21 +34,42 @@ function Home() {
       </div>
       <div className="d-flex flex-wrap mt-5 align-content-center justify-content-evenly">
         <div>
-          <form class="form">
-            <p class="form-title">Generate Short Url Here</p>
-            <div class="input-container">
-              <input placeholder="Enter url" type="email" required />
+          <form className="form">
+            <p className="form-title">Generate Short Url Here</p>
+            <div className="input-container">
+              <input
+                placeholder="Enter url"
+                type="text"
+                required
+                value={url}
+                onChange={(e) => {
+                  setUrl(e.target.value);
+                }}
+              />
             </div>
-            <div class="input-container">
-              <input placeholder="Enter short url or slug " type="text" />
+            <div className="input-container">
+              <input
+                placeholder="Enter short url (optional)"
+                type="text"
+                required
+                value={slug}
+                onChange={(e) => {
+                  setSlug(e.target.value);
+                }}
+              />
             </div>
-            <div class="input-container">
-              <input placeholder="Enter short " type="text" readOnly />
+            <div className="input-container">
+              <input type="text" readOnly value={shortUrl} ref={urlRef} />
               <span>
-                <img src={copyPng} alt="png" className="h-25"/>
+                <img
+                  src={copyPng}
+                  alt="png"
+                  className="h-25"
+                  onClick={copyShortUrl}
+                />
               </span>
             </div>
-            <button class="submit" type="button">
+            <button className="submit" type="button" onClick={generateLink}>
               Do Magic
             </button>
           </form>
