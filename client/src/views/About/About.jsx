6ae1f13bddb7dada15a432cc080ee90b544afb8c
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2023-11-18 16:08:18 +0530

    navbar added to about page

--- client/src/views/About/About.jsx
+++ client/src/views/About/About.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import "./About.css"
+import React from "react";
+import "./About.css";
+import Navbar from './../../components/Navbar/Navbar'
 function About() {
   return (
     <div>
+      <div className="sticky-top">
+        <Navbar />
+      </div>
       <h1>about</h1>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
