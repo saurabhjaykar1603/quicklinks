7ce086fc25a568dd0ce9e8e89cd7aae866ff0912
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2023-11-19 23:27:03 +0530

    ref : added qeuries to all links container and linkcard compo @778px

--- client/src/components/LinkCard/LinkCard.jsx
+++ client/src/components/LinkCard/LinkCard.jsx
@@ -3,23 +3,21 @@ import "./LinkCard.css";
 
 function LinkCard({ url, slug, clicks, i }) {
   return (
-    <div key={i} className="card py-2 px-3 mt-3 link-card-container  ">
-      <p className="m-0 mt-1">
-        Orginal Url : <span className="text-primary">{url}</span>{" "}
+    <div key={i} className="card py-2 px-3 mt-3 link-card-container">
+      <p className="m-0 mt-1  url">
+        Orginal Url : <span className="text-primary url">{url}</span>{" "}
       </p>
-      <p className="m-0 mt-1">
+      <p className="m-0 mt-1 url">
         Short Link :{" "}
-        <span className="text-primary" style={{ cursor: "pointer" }}>
+        <span className="text-primary url" style={{ cursor: "pointer" }}>
           {import.meta.env.VITE_API_BASE_URL}/api/{slug}
         </span>
       </p>
-      <hr className="m-0 my-3" />
-      <p className="m-0  fw-bold">
+      <hr className="m-0 my-2 my-sm-1" />
+
+      <p className="m-0 url fw-bold">
         {" "}
-        Clicks :
-        <span className=" rounded ms-2 fw">
-          {clicks}
-        </span>
+        Clicks :<span className="url rounded ms-2 fw">{clicks}</span>
       </p>
     </div>
   );
--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": "http://localhost:8000",
-      "/fetch": "http://localhost:8080",
+      "/fetch": "http://localhost:8000",
     },
   },
   plugins: [react()],
